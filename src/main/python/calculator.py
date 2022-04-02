304c1fe2a108cba86a9eca434bb6e88101579337
Borja Panadero <borparo@gmail.com>
2022-04-02 20:32:53 +0300

    switched literal for str(self.result) where used. Updated comments and docstring.

--- src/main/python/calculator.py
+++ src/main/python/calculator.py
@@ -1,3 +1,4 @@
+# calculator.py
 from PySide2.QtCore import Qt
 from PySide2.QtGui import QKeySequence
 from PySide2.QtWidgets import QWidget, QApplication, QLineEdit, QPushButton, QGridLayout, QVBoxLayout, QMessageBox
@@ -5,12 +6,23 @@ from collections import deque
 
 
 class Calculator(QWidget):
+    """
+    A simple Qt Calculator for learning purposes. This version stores values and operands in
+    separated queues and does the calculations when the operators or equal buttons are pressed,
+    displaying the result in the numbers field.
+    """
     main_window = QApplication.activeWindow()
 
     def __init__(self, parent=main_window):
         super(Calculator, self).__init__(parent)
         self.setWindowTitle("QtCalculator")
         self.setWindowFlags(self.windowFlags() ^ Qt.WindowContextHelpButtonHint)
+
+        self.values = deque()
+        self.operands = deque()
+        self.add_digit_state = True  # True for typing first digit in the field. False for inserting the following ones.
+        self.rounding = 4  # result digits to round for after decimal.
+        self.result = 0
 
         self.characters = ["C", "<-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".",
                            "="]
@@ -20,21 +32,14 @@ class Calculator(QWidget):
         self.create_layouts()
         self.create_connections()
 
-        self.values = deque()
-        self.operands = deque()
-        self.add_digit_state = True
-        self.rounding = 4
-        self.result = 0
-
     def create_widgets(self):
-        self.result_field = QLineEdit("0")
+        self.result_field = QLineEdit(str(self.result))
         self.result_field.setAlignment(Qt.AlignRight)
         self.result_field.setEnabled(False)
         self.result_field.setObjectName("result_field")
         self.buttons = [QPushButton(button) for button in self.characters]
         self.buttons_page = QWidget()
         for button in self.buttons:
-            # add shortcuts
             if button.text() == "C":
                 button.setShortcut(QKeySequence(Qt.Key_C))
                 button.setFixedSize(65, 65)
@@ -110,15 +115,15 @@ class Calculator(QWidget):
 
     def operation(self):
         """
-        Manages the operations within 2 queues: values and operands.
+        Manages and calculates the expressions for 2 stored values.
         """
-        # store values and operands in queues
+        # store values and operands for their use
         current_value = float(self.result_field.text())
 
         self.values.append(current_value)
         self.operands.append(self.sender().text())
 
-        # apply operand to values and update result
+        # with 2 values in queue, apply operand to them and update result accordingly the resulting expression.
         if len(self.values) == 2:
             if self.operands[0] == "+":
                 self.result = self.values[0] + self.values[1]
@@ -132,7 +137,7 @@ class Calculator(QWidget):
                 except ZeroDivisionError:
                     QMessageBox.warning(self, 'ZeroDivisionError',
                                         'Division by zero is not allowed.')
-            # clear queues of used calculations ( values and operands)
+            # Update and clear after calculating expression.
             self.values.clear()
             self.values.append(self.result)
             self.operands.popleft()
@@ -143,7 +148,7 @@ class Calculator(QWidget):
 
     def equal(self):
         """
-        Complete and display the final result of the operations.
+        Complete and display the final result of the operations queues.
         """
         self.operation()
         self.result_field.setText(str(round(self.result, self.rounding)))
@@ -165,10 +170,11 @@ class Calculator(QWidget):
         """
         Assigns to self.result the value written in the result_field.
         """
+        start_with_percent  = "0."
         if self.add_digit_state:
             # user can type smaller than 1 values starting with the decimal button.
             if self.sender().text() == '.':
-                self.result_field.setText("0.")
+                self.result_field.setText(start_with_percent)
             else:
                 self.result_field.setText(self.sender().text())
             self.add_digit_state = False
